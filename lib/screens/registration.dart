f785158b943c1aa4b00ec59d9e25038840d406ee
mikias-tulu <miki.tulu@gmail.com>
2023-03-31 21:53:14 +0300

    registration design change

--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ok_delivery/utils/style.dart';
 
 class Registration extends StatefulWidget {
   const Registration({super.key});
@@ -12,56 +13,95 @@ class _RegistrationState extends State<Registration> {
   bool? _agreedToContinue = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Enter your phone number to register:',
-                style: TextStyle(fontSize: 18.0),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _phoneController,
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                  hintText: 'Phone number',
-                  prefixIcon: Icon(Icons.phone),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                color: primaryColor,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.red,
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(50),
+                              topRight: Radius.circular(50)),
+                          color: secondaryColor),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Center(
+                              child: Image.asset(
+                            'assets/images/logo.png',
+                          )),
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              'Enter your phone number to register:',
+                              style: TextStyle(fontSize: 18.0),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: TextField(
+                              controller: _phoneController,
+                              keyboardType: TextInputType.phone,
+                              decoration: const InputDecoration(
+                                hintText: 'Phone number',
+                                prefixIcon: Icon(Icons.phone),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: CheckboxListTile(
+                              value: _agreedToContinue,
+                              onChanged: (newValue) {
+                                setState(() {
+                                  _agreedToContinue = newValue;
+                                });
+                              },
+                              title: const Text(
+                                'I agree to terms and conditions',
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                              controlAffinity: ListTileControlAffinity.leading,
+                            ),
+                          ),
+                          const SizedBox(height: 5.0),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ElevatedButton(
+                              onPressed: _agreedToContinue!
+                                  ? _continueButtonPressed
+                                  : null,
+                              child: const Text('Continue'),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: _agreedToContinue! ? _continueButtonPressed : null,
-                child: const Text('Continue'),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: CheckboxListTile(
-                value: _agreedToContinue,
-                onChanged: (newValue) {
-                  setState(() {
-                    _agreedToContinue = newValue;
-                  });
-                },
-                title: const Text(
-                  'I agree to continue',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
