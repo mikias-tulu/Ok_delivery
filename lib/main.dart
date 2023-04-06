73010802afa05db5e7e1fa3df6d12f878f003d0b
mikias-tulu <miki.tulu@gmail.com>
2023-04-06 16:16:13 +0300

    context builder change and added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:ok_delivery/screens/introduction_screen.dart';
+import 'package:ok_delivery/routes/routes.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ok_delivery/utils/strings.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ok Delivery',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: LayoutBuilder(
+        builder: (context, constraints) {
+          // Initialize ScreenUtil with the provided design size
+          ScreenUtil.init(
+            context,
+            designSize: const Size(414, 896),
+            //orientation: Orientation.portrait,
+          );
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: GetMaterialApp(
+                title: Strings.appName,
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  textTheme: GoogleFonts.robotoTextTheme(
+                    Theme.of(context).textTheme,
+                  ),
+                ),
+                navigatorKey: Get.key,
+                initialRoute: Routes.onboardScreen,
+                getPages: Routes.list,
+              ),
+            ),
+          );
+        },
       ),
-      home: const OnBoardingPage(),
     );
   }
 }
