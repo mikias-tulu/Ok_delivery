373bb3d794dfb358c4dc8e0796516fe97ce982f1
mikias-tulu <miki.tulu@gmail.com>
2023-04-14 23:49:52 +0300

    user state for authentication

---
+++ lib/controller/user_state.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:ok_delivery/screens/home/home_screen.dart';
+import 'package:ok_delivery/screens/login_signup_screen.dart';
+import 'package:ok_delivery/screens/map_screen.dart';
+
+class UserState extends StatelessWidget {
+  const UserState({Key? key}) : super(key: key);
+
+  static const String route = '/onboarding';
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    return user != null ? HomeScreen() : const LoginSignupScreen();
+  }
+}
