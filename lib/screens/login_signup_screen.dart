a5f87522c7ca6993d3ba5666ab7c9db292db58d4
mikias-tulu <miki.tulu@gmail.com>
2023-04-16 23:23:11 +0300

    email login and signup screen created

---
+++ lib/screens/login_signup_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:ok_delivery/utils/custom_style.dart';
+import 'package:ok_delivery/utils/strings.dart';
+
+import 'login_signup_screen/login_signup_form.dart';
+
+class LoginSignupScreen extends StatelessWidget {
+  const LoginSignupScreen({Key? key}) : super(key: key);
+
+  static const String route = '/login-signup';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.location_on_sharp,
+                  size: 100,
+                  color: Theme.of(context).primaryColor,
+                ),
+                const SizedBox(height: 10),
+                Text(Strings.appName,
+                    style: CustomStyle.dialogueTitleTextStyle),
+                const SizedBox(height: 20),
+                const LoginForm(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
