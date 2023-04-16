15492325454c957d325e069cf55cf623c5a37625
mikias-tulu <miki.tulu@gmail.com>
2023-04-16 23:22:48 +0300

    apikey changed

--- lib/config/map_api.dart
+++ lib/config/map_api.dart
@@ -1,1 +1,3 @@
-const String googleMapApi = 'AIzaSyBf4es4wWv_b0y8f3SqoWAbIb7dqqnnDUE';
+const String googleMapApi = 'AIzaSyBI5tnQWMSVsROc0vnn4ijC7FW0gj850_0';
+
+//AIzaSyBf4es4wWv_b0y8f3SqoWAbIb7dqqnnDUE
